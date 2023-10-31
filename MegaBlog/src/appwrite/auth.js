d0d22bb5fff796bcb537e7b0686a51562c74633d
ManasMajumdar <manasmajumdar889@gmail.com>
2023-10-31 15:26:04 +0530

    first commit

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,11 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    Client = new Client();
+    account 
+}
+
+const authService = new AuthService();
+
+export default authService
---
+++ MegaBlog/src/config/config.js
@@ -0,0 +1,13 @@
+//we make config file beacause of if we direct import env file in app.jsx so may be it cause some issue because in .env file there are may id which can be integer and string mixture but if if any id comes with an only integer value then its causes issue so in config file we have only sring value
+
+const config = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID)
+}
+
+export default config
+
+
