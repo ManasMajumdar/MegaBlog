fd0be727ad5926ab8a61b428a357a3eb21ca734b
ManasMajumdar <manasmajumdar889@gmail.com>
2023-10-31 15:43:09 +0530

    authbuild

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -2,8 +2,32 @@ import config from "../config/config";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    Client = new Client();
-    account 
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteURL)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+              // return userAccount;
+              //call another method
+            }
+            else{
+                return userAccount;
+            }
+        }catch(error){
+            throw error;
+        }
+    }
+
+    
 }
 
 const authService = new AuthService();
