53b9a7351882c748e2aaccc8082c5b8319b67cd2
ManasMajumdar <manasmajumdar889@gmail.com>
2023-11-29 17:47:44 +0530

    Production grade react component

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,7 +1,14 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Container from "./container/Container";
+
 
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
