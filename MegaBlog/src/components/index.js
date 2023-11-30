a5180873d86e00cd5a70a4a1c8e2fc75453b0525
ManasMajumdar <manasmajumdar889@gmail.com>
2023-11-30 11:23:25 +0530

    Adding Form and Slug Values

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -2,13 +2,14 @@ import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import RTE from "./RTE";
 import Container from "./container/Container";
-
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
